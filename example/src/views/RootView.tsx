eb063df654a02074b47c8c7dc9b53293a6e29202
mhp23 <hpousty@gmail.com>
2025-03-11 15:41:14 +0330

    chore(example): adding the highlighted text to the root view

--- example/src/views/RootView.tsx
+++ example/src/views/RootView.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import {gs} from '../styles/gs';
-import {Text, View} from 'react-native';
 import Button from '../components/Button';
-import Speech from '@mhpdev/react-native-speech';
+import {Alert, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Speech, {
+  HighlightText,
+  type HighlightedSegmentArgs,
+  type HighlightedSegmentProps,
+} from '@mhpdev/react-native-speech';
 
 const Introduction =
   "This high-performance text-to-speech library is built for bare React Native and Expo, compatible with Android and iOS's new architecture (default from React Native 0.76). It enables seamless speech management with start, pause, resume, and stop controls, and provides events for detailed synthesis management.";
 
 const RootView: React.FC = () => {
   const [isPaused, setIsPaused] = React.useState<boolean>(false);
+
   const [isStarted, setIsStarted] = React.useState<boolean>(false);
 
+  const [highlights, setHighlights] = React.useState<
+    Array<HighlightedSegmentProps>
+  >([]);
+
   React.useEffect(() => {
+    const onSpeechEnd = () => {
+      setIsStarted(false);
+      setIsPaused(false);
+      setHighlights([]);
+    };
+
     const startSubscription = Speech.onStart(({id}) => {
       console.log(`Speech ${id} started`);
     });
     const finishSubscription = Speech.onFinish(({id}) => {
-      setIsStarted(false);
-      setIsPaused(false);
+      onSpeechEnd();
       console.log(`Speech ${id} finished`);
     });
     const pauseSubscription = Speech.onPause(({id}) => {
@@ -29,24 +43,29 @@ const RootView: React.FC = () => {
       console.log(`Speech ${id} resumed`);
     });
     const stoppedSubscription = Speech.onStopped(({id}) => {
-      setIsStarted(false);
-      setIsPaused(false);
+      onSpeechEnd();
       console.log(`Speech ${id} stopped`);
     });
     const progressSubscription = Speech.onProgress(({id, location, length}) => {
+      setHighlights([
+        {
+          start: location,
+          end: location + length,
+        },
+      ]);
       console.log(
         `Speech ${id} progress, current word length: ${length}, current char position: ${location}`,
       );
     });
 
-    (async () => {
-      const enVoices = await Speech.getAvailableVoices('en-us');
-      Speech.initialize({
-        rate: 0.5,
-        volume: 1,
-        voice: enVoices[4]?.identifier,
-      });
-    })();
+    // (async () => {
+    //   const enVoices = await Speech.getAvailableVoices('en-us');
+    //   Speech.initialize({
+    //     rate: 0.5,
+    //     volume: 1,
+    //     voice: enVoices[3]?.identifier,
+    //   });
+    // })();
 
     return () => {
       startSubscription.remove();
@@ -68,11 +87,26 @@ const RootView: React.FC = () => {
     Speech.resume();
   }, []);
 
+  const onHighlightedPress = React.useCallback(
+    ({text, start, end}: HighlightedSegmentArgs) =>
+      Alert.alert(
+        'Highlighted',
+        `The current segment is "${text}", starting at ${start} and ending at ${end}`,
+      ),
+    [],
+  );
+
   return (
     <SafeAreaView style={[gs.flex, gs.p10]}>
       <View style={gs.flex}>
         <Text style={gs.title}>Introduction</Text>
-        <Text style={gs.paragraph}>{Introduction}</Text>
+        <HighlightText
+          text={Introduction}
+          style={gs.paragraph}
+          highlights={highlights}
+          highlightedStyle={styles.highlighted}
+          onHighlightedPress={onHighlightedPress}
+        />
       </View>
       <View style={[gs.row, gs.p10]}>
         <Button label="Start" disabled={isStarted} onPress={onStartPress} />
@@ -89,3 +123,10 @@ const RootView: React.FC = () => {
 };
 
 export default RootView;
+
+const styles = StyleSheet.create({
+  highlighted: {
+    fontWeight: '600',
+    backgroundColor: '#ffff00',
+  },
+});
